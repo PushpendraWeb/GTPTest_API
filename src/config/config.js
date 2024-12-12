9cc154134c5891f3a3b74bf195836f05522f8bcc
paya technology <paya.technology@gmail.com>
2024-12-12 14:02:29 +0530

    Update set all value

--- src/config/config.js
+++ src/config/config.js
@@ -4,14 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    DB_HOST: process.env.DB_HOST || 'kartitechdb.ccjfdjycfuj2.ap-south-1.rds.amazonaws.com',
-    DB_USER: process.env.DB_USER || 'admin',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'Kratitech666',
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
     DB_NAME: process.env.DB_NAME || 'GTP_TEST',
-    JWT_SECRET: process.env.JWT_SECRET || '1a2b3c4d5e6f7g8h9i0jklmnopqrstuvwxYz1234567890ABCDEFabcdefghiJklmNOpQ',
+    JWT_SECRET: process.env.JWT_SECRET,
     APP_MODE: process.env.APP_MODE || 'dev',
     PORT: process.env.PORT || 2000,
-    corsOrigin: ['http://13.127.167.24', 'http://localhost:4200']
+    corsOrigin: ['http://localhost:4200']
 }
 
 export default Object.freeze(config);
